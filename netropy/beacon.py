fd8e8ec700431d0fd822f33976643cb3f447bf9b
Cody Collier <cody@telnet.org>
2014-03-28 00:29:35 -0500

    early skeleton and design thinking

---
+++ netropy/beacon.py
@@ -0,0 +1,48 @@
+"""beacon
+
+
+
+"""
+
+
+record_fields = ['version', 'frequency', 'timeStamp',
+                 'seedValue', 'previousOutputValue',
+                 'signatureValue', 'outputValue', 'statusCode']
+
+Record = collections.namedtuple('Record', record_fields)
+
+
+def _timestamp():
+    pass
+
+
+def _get_current_timestamp():
+    pass
+
+
+def _parse_record():
+    pass
+
+
+def current():
+    pass
+
+
+def previous():
+    pass
+
+
+def next():
+    pass
+
+
+def last():
+    pass
+
+
+def start-chain():
+    pass
+
+
+
+
