4b4502aec6227d2fb9aaa200c60f4ab91598b989
Cody Collier <cody@telnet.org>
2014-04-02 23:15:28 -0500

    dev - adding template for beacon tests

---
+++ test/unit/test_beacon.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+
+import unittest
+
+import beacon
+
+
+class TestBeacon(unittest.TestCase):
+
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+
+    def test_true(self):
+        self.assertTrue(True)
+
+
+if __name__ == "__main__":
+
+    unittest.main()
