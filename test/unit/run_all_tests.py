457b49ee238e708db23ca9440b51e81e12f5a452
Cody Collier <cody@telnet.org>
2014-04-04 20:25:54 -0500

    adapt test runner from ocelog

---
+++ test/unit/run_all_tests.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+"""Run all of the unit tests
+
+This runner currently requires all test modules be imported and listed
+manually.  With each run, the order of the suites is randomized.  Further,
+the order of the tests inside each suite is also randomized.
+
+"""
+
+import unittest
+import random
+
+import test_auth
+import test_config
+import test_message
+import test_request_parsers
+import test_writer
+
+
+test_modules = (test_true, test_record, test_beacon)
+
+suite_list = []
+for testmod in test_modules:
+
+    suite = unittest.TestLoader().loadTestsFromModule(testmod)
+
+    # shuffle the list of tests in this suite
+    random.shuffle(suite._tests)
+    suite_list.append(suite)
+
+# shuffle the order of the suites
+random.shuffle(suite_list)
+
+all_suites = unittest.TestSuite(suite_list)
+unittest.TextTestRunner(verbosity=2).run(all_suites)
+
