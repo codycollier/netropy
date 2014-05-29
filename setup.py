0fa2e074f28eac96182696954418876fa5a93d0c
Cody Collier <cody@telnet.org>
2014-05-28 20:18:09 -0500

    updates to setup

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from distutils.core import setup
 setup (
     name = 'netropy',
     version = '1.0',
-    packages = 'netropy',
+    packages = ['netropy', ],
     long_description = open('README.md').read(),
 
     license = 'The MIT License (MIT)',
     author = 'Cody Collier',
+    author_email = 'cody@telnet.org',
     url = 'https://github.com/codycollier/netropy',
 )
 
